4fa10cb6cd95c8a87bd0cfd2c65b8a6a4e35076b
shakib-anwar-nibir17 <nibirshakib@gmail.com>
2024-02-14 20:24:50 +0600

    context for application

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import "./App.css";
 import Header from "./Components/Header/Header";
 import WeatherBoard from "./Components/Weather/WeatherBoard";
+import { WeatherProvider } from "./provider";
 
 function App() {
   return (
     <>
-      <div className="grid place-items-center h-screen">
-        <Header />
-        <main>
-          <section>
-            <WeatherBoard />
-          </section>
-        </main>
-      </div>
+      <WeatherProvider>
+        <div className="grid place-items-center h-screen">
+          <Header />
+          <main>
+            <section>
+              <WeatherBoard />
+            </section>
+          </main>
+        </div>
+      </WeatherProvider>
     </>
   );
 }
--- src/Components/Weather/WeatherHeadline.jsx
+++ src/Components/Weather/WeatherHeadline.jsx
@@ -1,16 +1,20 @@
+import { useContext } from "react";
 import PinImage from "../../assets/pin.svg";
+import { WeatherContext } from "../../context";
 const WeatherHeadline = () => {
+  const { weatherData } = useContext(WeatherContext);
+  const { climate, location, temperature, time } = weatherData;
   return (
     <div>
       <div className="max-md:flex items-center justify-between md:-mt-10">
         <img src="./assets/cloud.svg" alt="cloud" />
         <div className="max-md:flex items-center max-md:space-x-4">
           <h1 className="text-[60px] lg:text-[80px] xl:text-[100px] leading-none md:mb-4">
-            16°
+            {Math.round(temperature)}°
           </h1>
           <div className="flex items-center space-x-4 md:mb-4">
             <img src={PinImage} />
-            <h2 className="text-2xl lg:text-[50px]">Dhaka</h2>
+            <h2 className="text-2xl lg:text-[50px]">{location}</h2>
           </div>
         </div>
       </div>
